10a32e8bc3533eabc858a6fd34610a3bc6592e98
alice <89223922+tangalice@users.noreply.github.com>
2022-03-28 19:57:38 +0000

    updates

--- week2/factors.py
+++ week2/factors.py
@@ -9,9 +9,15 @@ class Factors:
         return self.factors
 
 def tester():
-    n = int(input("What number do you want to find the factors of? "))
+    #n = int(input("What number do you want to find the factors of? "))
     fac_of = Factors()
-    print(fac_of(n))
+    #print(fac_of(n))
+    print("Tester Number: 56")
+    print(fac_of(56))
+    print("Tester Number: 128")
+    print(fac_of(128))
+    print("Tester Number: 1024")
+    print(fac_of(1024))
 
 if __name__ == "__main__":
     tester()
